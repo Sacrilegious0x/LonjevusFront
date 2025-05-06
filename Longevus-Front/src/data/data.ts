87da2b0a980f4803cff3386afd72cf33cf4f06ee
Britany Villalobos <britany.villalobos@ucr.ac.cr>
2025-05-06 13:16:13 -0600

    Se agrego pagina de inicio

---
+++ Longevus-Front/src/data/data.ts
@@ -0,0 +1,13 @@
+export interface Slide {
+    id: number;
+    imgUrl: string;
+  }
+  
+  export const data: Slide[] = [
+    { id: 1, imgUrl: "/public/img/img1.png" },
+    { id: 2, imgUrl: "/public/img/img2.png" },
+    { id: 3, imgUrl: "/public/img/img3.png" },
+    { id: 4, imgUrl: "/public/img/img4.png" },
+    { id: 5, imgUrl: "/public/img/img5.png" }
+  ];
+  

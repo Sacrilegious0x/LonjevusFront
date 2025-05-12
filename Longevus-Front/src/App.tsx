2a91842d8a4c04316b537c87d22fd865cfbced8f
Gabriel Moya <itsgamc@gmail.com>
2025-05-11 22:36:38 -0600

    Fixed

--- Longevus-Front/src/App.tsx
+++ Longevus-Front/src/App.tsx
@@ -1,26 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import InventoryPage from "./pages/inventory/InventoryPage";
 import VisitSchedule from './pages/home/VisitSchedule';
 import Index from './pages/Employee/Index';
 import AddEmployee from './pages/Employee/AddEmployee';
-import ShowEmployee from './pages/Employee/ShowEmployee';
+import ShowEmployee from './pages/Employee/ShowEmployees';
+import EditEmployee from './pages/Employee/EditEmployee';
+import ViewEmployee from './pages/Employee/VIewEmployee';
 
 function App() {
   return (
     <BrowserRouter>  
       <Routes>
         <Route path="/" element={<HomePage />} />
-         <Route path="/inventario" element={<InventoryPage />} />
+        <Route path="/inventario" element={<InventoryPage />} />
         <Route path="/visita" element={<VisitSchedule />} />
         <Route path='/login' element={<Index/>} />
-        <Route path='/agregarEmpleado' element={< AddEmployee/>}/>
         <Route path='/agregar' element={< AddEmployee/>}/>
         <Route path='/mostrar' element={<ShowEmployee/>}/>
+        <Route path='/editar/:id' element={<EditEmployee/>}/>
+        <Route path='/perfil/:id' element={< ViewEmployee/>}/>
       </Routes>
     </BrowserRouter>
-    
   );
 }
-
 export default App;

894f1d932248f59b7d90b0b1d9cf8d9965e231e6
Pablo Ramirez <pablo18jru@gmail.com>
2025-05-11 19:20:44 -0600

    Se modifico la vista para mostrar los proveedores con el componente agregado _Table_

--- Longevus-Front/src/App.tsx
+++ Longevus-Front/src/App.tsx
@@ -5,6 +5,10 @@ import VisitSchedule from './pages/home/VisitSchedule';
 import Index from './pages/Employee/Index';
 import AddEmployee from './pages/Employee/AddEmployee';
 import ShowEmployee from './pages/Employee/ShowEmployee';
+import SuppliersAdd from './pages/suppliers/SuppliersAdd';
+import Role_Permissions from './pages/suppliers/Role_permission';
+import SuppliersEdit from './pages/suppliers/SuppliertsUpdate';
+import SuppliersList from './pages/suppliers/SuppliersList';
 
 function App() {
   return (
@@ -17,6 +21,14 @@ function App() {
         <Route path='/login' element={<Index/>} />
         <Route path='/agregar' element={< AddEmployee/>}/>
         <Route path='/mostrar' element={<ShowEmployee/>}/>
+
+
+        <Route path="/proveedores" element={<SuppliersList />} />
+        <Route path="/proveedores/guardar" element={<SuppliersAdd/>} />
+        <Route path="/roles_permisos" element={<Role_Permissions />} />
+        <Route path="/proveedores/editar" element={<SuppliersEdit/>} />
+
+
       </Routes>
     </Router>
   );
---
+++ Longevus-Front/src/pages/suppliers/SuppliersList.tsx
@@ -0,0 +1,83 @@
+
+import { href } from "react-router-dom";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import type {columnDefinition} from '../../components/TableBasic';
+import { Link } from 'react-router-dom';
+import Table from '../../components/TableBasic';
+
+interface ISupplier{
+    id: number,
+    name: string,
+    phoneNumber: string,
+    email: string,
+    address: string,
+    photo: string,
+    isActive: number
+}
+
+const supplierData: ISupplier[] = [
+  {id: 1, name: 'Tilin',phoneNumber:'86784783' ,email: 'alice@example.com', address:'Ticaban 200 mts norte de la iglesia',photo:'urlexample',isActive:1},
+  {id: 2, name: 'Varela',phoneNumber:'86784783' ,email: 'blabla@example.com', address:'Ticaban 200 mts norte de la iglesia',photo:'urlexample',isActive:1}
+];
+
+
+const SuppliersList = () =>{
+
+   const supplierColumns: columnDefinition<ISupplier>[] =[
+    {header: '#', accessor: 'id', Cell:(supplier, index)=>{return(index+1)}},
+    {header: 'Nombre', accessor: 'name'},
+    {header: 'Teléfono', accessor: 'phoneNumber'},
+    {header: 'Correo', accessor: 'email'},
+    {header: 'Direccion', accessor: 'address'},
+    {header: 'Foto', accessor: 'photo'},
+    {header: 'Activo', accessor: 'isActive'},
+
+    {header: 'Acciones', accessor: (supplier) => supplier,   
+        Cell: (supplier) =>(
+            <>
+            <a className='btn btn-info me-2' onClick={()=>console.log("Mostrar"+supplier.id)}>
+                <i className='bi bi-eye'/>
+            </a>
+            <a className='btn btn-warning me-2' onClick={()=>console.log("Editar"+supplier.name)}>
+                <i className='bi bi-pencil-square'/>
+            </a>
+            
+            <a className='btn btn-danger me-2' onClick={()=>console.log("Eliminar"+supplier.id)}>
+                <i className="bi bi-trash"/>
+            </a>
+            
+            </>
+        )
+        
+    }
+   ];
+
+return (
+
+    
+  <>  
+    <Header/>
+      <div className="container ">
+        <div className='row'>
+            <div className='card mt-5 mb-5'>
+                <div className='card-title d-flex justify-content-between align-items-center mt-3'>
+                        <h4 className="m-2">Lista de proveedores</h4>
+                        <Link className='btn btn-success' to='/proveedores/guardar'>Agregar</Link>
+                </div>  
+                <div className='card-body'>
+                        <input className="mb-3" type="text" placeholder="Buscar..." id="supplierSearch"/>
+                        <button className="btn btn-secondary" id="btnSearch"><i className='bi bi-search'/></button>
+                        <Table<ISupplier> data={supplierData} columns={supplierColumns} selectedRows={new Set()} onToggleRow={()=>{}} onSelectAll={()=>{}}/>
+                </div>
+            </div>
+        </div>
+      </div>
+    <Footer/>
+  </>  
+    
+    )
+  }
+
+  export default SuppliersList;
+  

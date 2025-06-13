311f217c7d15aac8b0b1ef76353ba74cc0fde56f
Pablo Ramirez <pablo18jru@gmail.com>
2025-06-13 16:50:49 -0600

    Mod

--- Longevus-Front/src/pages/product/AddProduct.tsx
+++ Longevus-Front/src/pages/product/AddProduct.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useAsyncError, useNavigate } from "react-router-dom";
+import {  useNavigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { createProduct, getUnits, type IUnit } from "../../services/ProductService";
